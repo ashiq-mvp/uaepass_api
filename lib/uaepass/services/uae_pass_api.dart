b8376e3cb3e8e9dff5f984c8eb28794340e07aa5
Aswathy Gireesh <aswathy@mvp-apps.ae>
2025-04-02 15:55:34 +0400

    code update

--- lib/uaepass/services/uae_pass_api.dart
+++ lib/uaepass/services/uae_pass_api.dart
@@ -88,6 +88,7 @@ class UaePassAPI {
               urlScheme: _appScheme,
               isProduction: _isProduction,
             ),
+            fullscreenDialog: fullscreen,
           ),
         );
       }
